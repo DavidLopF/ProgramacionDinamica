08c15c3cdba28dcc3b9189ea46fabad9f64d773a
gitForDavid <dlopezf@unbosque.edu.co>
2021-11-09 16:15:48 -0500

    floyd terminado

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -24,16 +24,18 @@ public class Controller {
 			int arcos = vista.capturarInt("Ingrese cantidad de vertices que va a tener el grafo:  ");
 			int nodos = vista.capturarInt("Ingrese cantidad de nodos para crear aleatoriamente: ");
 
-			if (arcos < nodos) {
-				vista.mostrarMensaje("Los vertices no pueden ser mayores que los nodos. ");
-				funcionar();
+			try {
+				grafo = new Grafo(arcos, nodos);
+				grafo.ingresarArco();
+				vista.mostrarMensaje("¡¡¡ Grafo creado con exito !!!\n" + "Matriz de adyacencia:\n"
+						+ grafo.mostrarMatriz(grafo.getMatrizAdy()) + "\n matriz de pesos:\n"
+						+ grafo.mostrarMatriz(grafo.getPesos()));
+
+				vista.mostrarMensaje("Mostrar resultados de algoritmo de floyd: \n" + grafo.floyd(grafo.getMatrizAdy()));
+			} catch (Exception e) {
+				vista.mostrarMensaje("Valide datos. ");
 			}
-			
-			grafo = new Grafo(arcos, nodos);
-			grafo.ingresarArco();
-			vista.mostrarMensaje(grafo.mostrarMatrizAdy());
-			
-			
+
 			break;
 		}
 
--- src/co/edu/unbosque/view/View.java
+++ src/co/edu/unbosque/view/View.java
@@ -3,49 +3,52 @@ package co.edu.unbosque.view;
 import javax.swing.JOptionPane;
 
 public class View {
-	
-	 public void mostrarMensaje(String a) {
-	        JOptionPane.showMessageDialog(null, a);
-	    }
 
-	    public String capturarString(String titulo) {
-	        String a = JOptionPane.showInputDialog(titulo);
-	        return a;
-	    }
+	public void mostrarMensaje(String a) {
+		JOptionPane.showMessageDialog(null, a);
+	}
 
-	    public int capturarInt(String titulo) {
-	        String a = JOptionPane.showInputDialog(titulo);
-	        int num = 0;
-	        if (esNumeroInt(a)) {
-	            num = Integer.parseInt(a);
-	            System.out.println("DATO INGRESADO CON EXITO.");
-	        } else {
-	            System.out.println("ERROR.. NO ES ENTERO.");
-	            capturarInt(titulo);
-	        }
+	public String capturarString(String titulo) {
+		String a = JOptionPane.showInputDialog(titulo);
+		return a;
+	}
 
-	        return num;
-	    }
+	public int capturarInt(String titulo) {
+		String a = JOptionPane.showInputDialog(titulo);
+		int num = 0;
+		while (!esNumeroInt(a)) {
 
+			System.out.println("ERROR.. NO ES ENTERO.");
+			a = JOptionPane.showInputDialog(titulo);
+			num = 0;
 
-	    private boolean esNumeroInt(String mensaje) {
-	        try {
-	            Integer.parseInt(mensaje);
-	            return true;
-	        } catch (NumberFormatException nfe) {
-	            return false;
-	        }
-	    }
+		}
+		num = Integer.parseInt(a);
+		System.out.println("DATO INGRESADO CON EXITO.");
 
-	    public String menuPrincipal() {
-	        Object[] opciones = {"1. Algoritmo de floyd.", "2. Problema del viajero","3. Multipicacion de matrices"
-	        		,"4. Problema de asignación de tareas", "5. Coeficiente binomiales", "6. Salir"};
-	        Object opcion = JOptionPane.showInputDialog(null, "      ..:Bienvenido señor usuario :..                " + "\n\n\nSelecciona un operacion a realizar:    ", "Elegir",
-	                JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
-	        if (opcion == null) {
-	            mostrarMensaje("Hasta Pronto");
-	        }
-	        return opcion.toString();
-	    }
+		return num;
+	}
+
+	private boolean esNumeroInt(String mensaje) {
+		try {
+			Integer.parseInt(mensaje);
+			return true;
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+	}
+
+	public String menuPrincipal() {
+		Object[] opciones = { "1. Algoritmo de floyd.", "2. Problema del viajero", "3. Multipicacion de matrices",
+				"4. Problema de asignación de tareas", "5. Coeficiente binomiales", "6. Salir" };
+		Object opcion = JOptionPane.showInputDialog(null,
+				"      ..:Bienvenido señor usuario :..                "
+						+ "\n\n\nSelecciona un operacion a realizar:    ",
+				"Elegir", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+		if (opcion == null) {
+			mostrarMensaje("Hasta Pronto");
+		}
+		return opcion.toString();
+	}
 
 }
